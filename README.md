2251b7f8f2aa084068f0af37882577fe84084c5c
Ku Jia Yang <118326100+kujiayang@users.noreply.github.com>
2023-10-08 22:02:36 +0800

    dsc

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Relative Stability: pegged to US $1
+   1. Chainlink Price feed.
+   2. Set a function to exchange ETH and BTC -> $$$
+2. Stability Mechanism: algorithmic (decentralized)
+   1. People can only mint stablecoin with enough collateral
+3. Collateral: exogenous (crypto)
+   1. wETH
+   2. wBTC
---
+++ test/fuzz/Invariants.t.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+// List of invariants aka properties that hold true all the time
+// 1. Total supply of DSC should be less than value of collateral
+// 2. Getter view functions should never revert <- evergreen invariant
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Handler} from "./Handler.t.sol";
+
+contract Invariants is StdInvariant, Test {
+    DeployDSC deployer;
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    address weth;
+    address wbtc;
+    Handler handler;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+        (dsc, dsce,config) = deployer.run();
+        //targetContract(address(dsce));
+        handler = new Handler(dsce, dsc);
+        targetContract(address(handler));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        // total supply of all DSC
+        uint256 totalSupply = dsc.totalSupply();
+        // total weth in contract
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dsce.getUsdValue(wbtc, totalWbtcDeposited);
+
+        console.log("weth value: ", wethValue);
+        console.log("wbtc value: ", wbtcValue);
+        console.log("total supply: ", totalSupply);
+        console.log("Times mint called: ", handler.timesMintIsCalled());
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+
+    // should always be included
+    function invariant_gettersShouldNotRevert() public view {
+
+
+    }
+}
